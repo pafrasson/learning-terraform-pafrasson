80fecd9d1aac6f196247463eac06c109ec4073de
Pedro Frasson <96358377+pafrasson@users.noreply.github.com>
2025-06-03 11:02:19 -0300

    feat: https sec group

--- main.tf
+++ main.tf
@@ -22,6 +22,8 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
+  vpc_security_group_ids = [aws_security_group.blog.id]
+
   tags = {
     Name = "HelloWorld"
   }
@@ -44,11 +46,21 @@ resource "aws_security_group_rule" "blog_http_in" {
   security_group_id = aws_security_group.blog.id
 }
 
-resource "aws_security_group_rule" "blog_http_in" {
+resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
-  from_port   = 80
-  to_port     = 80
+  from_port   = 443
+  to_port     = 443
   protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "aws_security_group_rule" "blog_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
